6e05c7e926e9ce5dae02dad0036e84dcc62c8f41
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-03-12 00:00:46 +0100

    styled product page

--- src/pages/ProductPage/index.jsx
+++ src/pages/ProductPage/index.jsx
@@ -1,21 +1,40 @@
-import { useEffect } from "react";
 import {useParams } from "react-router-dom"
 import { baseURL } from "../../utilities/constants";
 import { useAPI } from "../../hooks/API";
 import { updateHead } from "../../utilities/updateHead";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, removeFromCart, decreaseQuantity, clearCart } from "../../context/cartSlice";
+import { PageGrid, Wrapper, DetailsContainer, Divider, ImageContainer, ProductImage, ProductSaleBanner, Heading1, Heading2, RatingStar, RatingLink, ButtonAddCart,IconImage, ButtonAddFavourite, CurrentPrice, PreviousPrice, PreviousPriceAmount } from "./styles";
+import fullStar from "../../assets/review_star.png"
+import emptyStar from "../../assets/empty_review_star.png"
+import cartIcon from "../../assets/shopping_cart_add.png"
 
-function ProductSpecific(){
+
+function RatingStars({ rating, reviews }) {
+  const full = parseInt(Math.round(rating));
+  const empty = 5 - full;
+  const stars = [];
+
+  for (let i = 0; i < full; i++) {
+    stars.push(<RatingStar src={fullStar} alt="rating star icon" key={`full-${i}`} />);
+  }
+  for (let i = 0; i < empty; i++) {
+    stars.push(<RatingStar src={emptyStar} alt="rating star icon" key={`empty-${i}`} />);
+  }
+
+  return (
+    <p>
+      Rating: {stars} ({reviews && reviews.length === 0 ? "Unrated" : rating})
+    </p>
+  );
+}
+
+
+function ProductPage(){
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   const params = useParams();
   const {data, isLoading, isError} = useAPI((baseURL+ params.id));
-
-  useEffect(() => {
-    console.log(cart);
-  }, [cart]);
-
   
   if(isLoading){
     return (
@@ -29,34 +48,41 @@ function ProductSpecific(){
 
   updateHead(data.title, data.description)
   return(
-    <>
-      <h1>{data.title}</h1>
-      <img src={data.imageUrl} alt={data.title} />
-      <p>{data.description}</p>
-      <p>Price: {data.discountedPrice < data.price ? `kr ${data.discountedPrice}, Save kr ${data.price-data.discountedPrice}`: "kr "+ data.discountedPrice}</p>
-      <button onClick={() => dispatch(addToCart({quantity:1, product:data}))}>Add To Cart</button>
-      <button onClick={() => dispatch(removeFromCart({product:data}))}>remove</button>
-      <button onClick={() => dispatch(decreaseQuantity({quantity:1, product:data}))}>decrease</button>
-      <button onClick={() => dispatch(clearCart())}>clear</button>
-      <p>Categories: {data.tags ? data.tags.join(', ') : 'None'}</p>
-      <h2>Reviews</h2>
-      <p>Rating: {data.rating}</p>
+    <Wrapper>
+      <Heading1>{data.title}</Heading1>
+      <RatingLink href="#reviews">
+        <RatingStars rating={data.rating} reviews={data.reviews}/>
+      </RatingLink>
+      <PageGrid>
+        <ImageContainer>
+          <ProductImage src={data.imageUrl} alt={data.title} />
+          {data.discountedPrice < data.price ? <ProductSaleBanner>Save {Math.round(((data.price- data.discountedPrice) /data.price)*100)}%</ProductSaleBanner>:""}
+        </ImageContainer>
+        <DetailsContainer>
+          <Heading2>Product Brief</Heading2>
+          <p>{data.description}</p>
+          <Divider/>
+          {data.discountedPrice < data.price ? <PreviousPrice>Original Price: <PreviousPriceAmount>kr {data.price}</PreviousPriceAmount></PreviousPrice>: ""}
+          <CurrentPrice>Current Price: {data.discountedPrice < data.price ? `kr ${data.discountedPrice}, Save kr ${data.price-data.discountedPrice}`: "kr "+ data.discountedPrice}</CurrentPrice>
+          <Divider/>
+          <ButtonAddCart onClick={() => dispatch(addToCart({quantity:1, product:data}))}><IconImage src={cartIcon} alt="cart icon"/>Add To Cart</ButtonAddCart>
+          <Divider/>
+          <p>Categories: {data.tags ? data.tags.join(', ') : 'None'}</p>
+          <button onClick={() => dispatch(removeFromCart({product:data}))}>remove</button>
+          <button onClick={() => dispatch(decreaseQuantity({quantity:1, product:data}))}>decrease</button>
+          <button onClick={() => dispatch(clearCart())}>clear</button>
+        </DetailsContainer>
+      </PageGrid>
+      <Heading2 id="reviews">Reviews</Heading2>
       <div>
-        {data.reviews ? data.reviews.map((review) => <div key={review.id}>
+        {data.reviews && data.reviews.length > 0 ? data.reviews.map((review) => <div key={review.id}>
+          <Divider/>
           <h3>{review.username}</h3>
           <p>{review.description}</p>
-          <p>Rating: {review.rating}</p>
-        </div>) : <p>No Reviews Yet.</p>}
+          <RatingStars rating={review.rating} reviews={data.reviews}/>
+        </div>) : <p>No reviews yet.</p>}
       </div>
-    </>
-  )
-}
-
-function ProductPage(){
-  return(
-    <main>
-      <ProductSpecific />
-    </main>
+    </Wrapper>
   )
 }
 
--- src/pages/ProductPage/styles.jsx
+++ src/pages/ProductPage/styles.jsx
@@ -1,8 +1,131 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
-  margin: 0 auto;
+  margin: 90px auto 2rem auto;
   padding: 0 1rem;
-  max-width: 1600px;
+  max-width: 1400px;
+  @media (min-width: 700px) {
+    padding: 0 2rem;
+  }
 `;
 
+export const Heading1 = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 600;
+  @media (min-width: 700px) {
+    font-size: 2rem;
+  }
+`
+
+export const RatingStar = styled.img`
+  width: 1.5rem;
+  margin-bottom: -4px;
+`
+
+export const RatingLink = styled.a`
+  color: black;
+  text-decoration: none;
+  :hover{
+    text-decoration: underline;
+  }
+`
+export const PageGrid = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: minmax(0, 1fr);
+  grid-gap: 2rem;
+  margin: 0 auto;
+  @media (min-width: 700px) {
+    grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+  }
+`
+
+export const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  background-color: var(--tertiary);
+  overflow: hidden;
+  border-radius: 3rem;
+  overflow: hidden;
+  height: fit-content;
+`
+
+export const ProductImage = styled.img`
+  width: 100%;
+  object-fit: contain;
+  height: 400px;
+  @media (min-width: 700px) {
+    height: 40vw;
+    max-height: 600px;
+  } 
+`
+
+export const ProductSaleBanner = styled.div`
+  width: 100%;
+  padding: 0.5rem;
+  background-color: var(--secondary);
+  font-size: 1.5rem;
+  text-align: center;
+  position: absolute;
+  top: 10%;
+  left: -35%;
+  transform: rotate(-40deg);
+`
+
+export const DetailsContainer = styled.div`
+  max-width: 500px;
+`
+
+export const Heading2 = styled.h2`
+  font-size: 1.25rem;
+  font-weight: 600;
+  @media (min-width: 700px) {
+    font-size: 1.5rem;
+  }
+`
+
+export const CurrentPrice = styled.p`
+  font-weight: 600;
+`
+
+export const PreviousPrice = styled.p`
+  
+`
+
+export const PreviousPriceAmount = styled.span`
+  text-decoration: line-through;
+  font-style: italic;
+`
+
+export const ButtonAddCart = styled.button`
+  cursor: pointer;
+  display: flex;
+  flex-direction:row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  font-size: 1.125rem;
+  padding: 0.5rem;
+  border-radius: 1rem;
+  border: none;
+  background-color: green;
+  color: white;
+  margin-bottom: 1rem;
+  border: 1px solid green;
+  transition: all 0.5s;
+  :hover{
+    background-color: white;
+    color: green;
+  }
+`
+
+export const IconImage = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+`
+
+export const Divider = styled.hr`
+  color: var(--secondary);
+  margin: 2rem 0;
+`
