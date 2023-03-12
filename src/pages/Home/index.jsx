31bee2363635ee0afb67773b47aab88dc96c9a94
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-03-12 13:20:26 +0100

    Setup product store to prevent constant fetching

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,37 @@
 import { baseURL } from "../../utilities/constants";
-import { useAPI } from "../../hooks/API";
 import  ProductCard  from "../../components/ProductCard";
 import  ProductCardLoader  from "../../components/ProductLoader";
 import { Banner, HomeHeading, ProductGrid, PageWrapper, Main, FilterContainer,Filter, HiddenLabel, SearchContainer} from "./styles";
 import bannerImage from "../../assets/banner.jpg"
 import { updateHead } from "../../utilities/updateHead";
 import Search from "../../components/Search";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchingProducts, fetchedProducts, fetchFailed } from '../../context/productsSlice';
+
 
 function Products() { 
-  const {data, isLoading, isError} = useAPI(baseURL);
-  if(!isLoading){
-    console.log(data)
-  }
+  const dispatch = useDispatch();
+  const {data, isLoading, isError} = useSelector((state) => state.products);
+
+  //prevent constant reloading
+  useEffect(() => {
+    if(data.length === 0){
+      const getProducts = async () => {
+        try{
+          dispatch(fetchingProducts());
+          const res = await fetch(baseURL);
+          const data = await res.json();
+          dispatch(fetchedProducts(data))
+          console.log(data)
+        } catch {
+          dispatch(fetchFailed());
+        }
+      }
+      getProducts()
+
+    }
+  }, [dispatch, data])
 
   if (isLoading) {
     return <ProductGrid>
