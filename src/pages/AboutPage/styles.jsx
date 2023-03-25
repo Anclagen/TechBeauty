a2276fc7bdc5aac1138330702a7431436de16745
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-03-25 22:54:59 +0100

    added parallax images to about

--- src/pages/AboutPage/styles.jsx
+++ src/pages/AboutPage/styles.jsx
@@ -1,17 +1,60 @@
 import styled from "styled-components";
+import bannerImage from "../../assets/product_banner_display1.jpg";
+import productImage from "../../assets/product_banner_display2.jpg";
 
-export const AboutContentWrapper = styled.main`
+export const AboutMain = styled.main``;
+
+export const AboutContentWrapper = styled.div`
   max-width: 700px;
   padding: 2rem;
   border-radius: 1rem;
   background-color: ${({ theme }) => theme.color.tertiary};
-  margin: 110px 2rem 2rem 2rem;
+  margin: 1rem 2rem 2rem 2rem;
 
-  h1 {
-    text-align: center;
+  div {
+    background-image: url(${productImage});
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 300px;
   }
 
   @media (min-width: 700px) {
-    margin: 110px auto 2rem auto;
+    margin: 1rem auto 2rem auto;
   }
+`;
+
+export const AboutHeader = styled.div`
+  position: relative;
+  margin: 0;
+  overflow: hidden;
+  min-height: 65dvh;
+
+  h1 {
+    font-size: 1.5rem;
+
+    padding: 0.5rem;
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.shadow.popup};
+    text-align: center;
+    white-space: nowrap;
+    position: absolute;
+    background-color: ${({ theme }) => theme.color.primary};
+    color: ${({ theme }) => theme.color.secondary};
+    margin: 2rem;
+    z-index: 10;
+    left: 49%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    @media (min-width: 700px) {
+      font-size: 2.5rem;
+    }
+  }
+
+  background-image: url(${bannerImage});
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
--- src/pages/ProductPage/styles.jsx
+++ src/pages/ProductPage/styles.jsx
@@ -119,25 +119,3 @@ export const Divider = styled.hr`
   color: ${({ theme }) => theme.color.secondary};
   margin: 2rem 0;
 `;
-
-const slideIn = keyframes`
-  0% {top: 0px;}
-  30% { top: 170px;}
-  70% { top: 170px;}
-  100% { top: 0px;}
-`;
-
-export const Popup = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: darkgreen;
-  color: white;
-  font-size: 1.25rem;
-  padding: 1.5rem 2rem;
-  border-radius: 1rem;
-  z-index: 10;
-  box-shadow: ${({ theme }) => theme.shadow.popup};
-  animation: ${slideIn} 3s ease-out forwards;
-`;
